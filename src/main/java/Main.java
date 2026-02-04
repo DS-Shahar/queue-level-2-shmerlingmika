e6f619edb6368136c9a6caf788cad77fa4716667
shmerlingmika <shmerling.mika@gmail.com>
2026-02-05 00:09:47 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -343,12 +343,18 @@ public class Main {
 	static Scanner reader=new Scanner(System.in);
 	public static void main(String[] args) {
 	    
-	    BinNode root = new BinNode(8);
-        root.setLeft(new BinNode(4));
-        root.setRight(new BinNode(10));
-        root.getLeft().setLeft(new BinNode(2));
-        root.getLeft().setRight(new BinNode(6));
+	    BinNode root = new BinNode(1);
+        root.setLeft(new BinNode(2));
+        root.setRight(new BinNode(1));
+        root.getLeft().setLeft(new BinNode(4));
+        root.getLeft().setRight(new BinNode(3));
         System.out.println(root);
+        BinNode root2 = new BinNode(8);
+        root2.setLeft(new BinNode(4));
+        root2.setRight(new BinNode(10));
+        root2.getLeft().setLeft(new BinNode(2));
+        root2.getLeft().setRight(new BinNode(6));
+        System.out.println(root2);
         //ex11a(root);
         //System.out.println(ex11b(root));
         //System.out.println(ex11c(root));
@@ -356,7 +362,11 @@ public class Main {
         //System.out.println(ex12(root));
         //System.out.println(ex14(root));
         //System.out.println(ex16(root));
-        System.out.println(ex17(root));
+        //System.out.println(ex17(root));
+        //System.out.println(ex1(root));
+        //System.out.println(ex2(root));
+        //System.out.println(ex18(root,root2));
+        System.out.println(ex20(root,4));
 
 	}
 
@@ -427,8 +437,8 @@ public class Main {
         else    
             return true;
     }
-
-	public static int ex12(BinNode<Integer> root) 
+    
+    public static int ex12(BinNode<Integer> root) 
 	{
         if (root == null)
             return 0;
@@ -448,6 +458,34 @@ public class Main {
             return ex14(root.getLeft()) + ex14(root.getRight()) + 1;
         else    
             return ex14(root.getLeft()) + ex14(root.getRight());
+    }
+    
+    public static boolean ex1(BinNode<Integer> root) 
+	{
+        if (root == null)
+            return true;
+        
+        if (!(root.hasRight() || root.hasLeft()))
+        {
+            if (!(root.getValue()%2==0))
+                return false;
+        }
+        
+        return (ex1(root.getLeft()) && ex1(root.getRight()));
+    }
+    
+    public static boolean ex2(BinNode<Integer> root) 
+	{
+        if (root == null)
+            return true;
+        
+        if (root.hasRight())
+        {
+            if (!(root.hasLeft()))
+                return false;
+        }
+        
+        return (ex2(root.getLeft()) && ex2(root.getRight()));
     }
     
     public static int ex16(BinNode<Integer> root) 
@@ -476,7 +514,58 @@ public class Main {
         else    
             return ex17(root.getLeft()) + ex17(root.getRight());
     }
-
-	
-
+    
+    public static boolean ex18he(BinNode<Integer> t1,int num) 
+	{  
+        if (t1 == null)
+            return false;
+            
+        if (t1.getValue()==num)
+        {
+            return true;
+        }
+        
+        return (ex18he(t1.getLeft(),num) || ex18he(t1.getRight(),num));
+    }
+    
+    public static boolean ex18(BinNode<Integer> t1,BinNode<Integer> t2) 
+	{
+        if (t2 == null)
+            return true;
+            
+        if (!ex18he(t1,t2.getValue()))
+        {
+            return false;
+        }
+        
+        return (ex18(t1,t2.getLeft()) && ex18(t1,t2.getRight()));
+    }
+    
+    public static boolean ex20he(BinNode<Integer> t1,int num) 
+	{  
+        if (t1 == null)
+            return false;
+            
+        if (t1.getValue()==num)
+        {
+            return !(ex20he(t1.getLeft(),num) || ex20he(t1.getRight(),num));
+        }
+        
+        return (ex20he(t1.getLeft(),num) || ex20he(t1.getRight(),num));
+    }
+    
+    public static boolean ex20(BinNode<Integer> t1,int num) 
+	{
+        if (num == 0)
+            return true;
+            
+        if (!ex20he(t1,num))
+        {
+            return false;
+        }
+        
+        return ex20(t1,num-1);
+    }
+    
+    
 }
